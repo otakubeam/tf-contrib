otakubeam/tf-contrib

commit 940ed77590739f524d1478056c74e93cd5f59c7f

Convert cells to OO-based to reduce call() overhead
PiperOrigin-RevId: 170898081

tensorflow/contrib/rnn/python/ops/rnn_cell.py
@@ -1017,7 +1017,7 @@ class BidirectionalGridLSTMCell(GridLSTMCell):
 
 
 # pylint: disable=protected-access
-_linear = rnn_cell_impl._linear
+_Linear = rnn_cell_impl._Linear  # pylint: disable=invalid-name
 # pylint: enable=protected-access
 
 
@@ -1079,6 +1079,9 @@ class AttentionCellWrapper(rnn_cell_impl.RNNCell):
     self._attn_size = attn_size
     self._attn_length = attn_length
     self._reuse = reuse
+    self._linear1 = None
+    self._linear2 = None
+    self._linear3 = None
 
   @property
   def state_size(self):
@@ -1110,7 +1113,9 @@ class AttentionCellWrapper(rnn_cell_impl.RNNCell):
     input_size = self._input_size
     if input_size is None:
       input_size = inputs.get_shape().as_list()[1]
-    inputs = _linear([inputs, attns], input_size, True)
+    if self._linear1 is None:
+      self._linear1 = _Linear([inputs, attns], input_size, True)
+    inputs = self._linear1([inputs, attns])
     cell_output, new_state = self._cell(inputs, state)
     if self._state_is_tuple:
       new_state_cat = array_ops.concat(nest.flatten(new_state), 1)
@@ -1118,7 +1123,9 @@ class AttentionCellWrapper(rnn_cell_impl.RNNCell):
       new_state_cat = new_state
     new_attns, new_attn_states = self._attention(new_state_cat, attn_states)
     with vs.variable_scope("attn_output_projection"):
-      output = _linear([cell_output, new_attns], self._attn_size, True)
+      if self._linear2 is None:
+        self._linear2 = _Linear([cell_output, new_attns], self._attn_size, True)
+      output = self._linear2([cell_output, new_attns])
     new_attn_states = array_ops.concat(
         [new_attn_states, array_ops.expand_dims(output, 1)], 1)
     new_attn_states = array_ops.reshape(
@@ -1141,7 +1148,9 @@ class AttentionCellWrapper(rnn_cell_impl.RNNCell):
       hidden = array_ops.reshape(attn_states,
                                  [-1, self._attn_length, 1, self._attn_size])
       hidden_features = conv2d(hidden, k, [1, 1, 1, 1], "SAME")
-      y = _linear(query, self._attn_vec_size, True)
+      if self._linear3 is None:
+        self._linear3 = _Linear(query, self._attn_vec_size, True)
+      y = self._linear3(query)
       y = array_ops.reshape(y, [-1, 1, 1, self._attn_vec_size])
       s = reduce_sum(v * tanh(hidden_features + y), [2, 3])
       a = softmax(s)
@@ -1537,6 +1546,7 @@ class UGRNNCell(rnn_cell_impl.RNNCell):
     self._forget_bias = forget_bias
     self._activation = activation
     self._reuse = reuse
+    self._linear = None
 
   @property
   def state_size(self):
@@ -1573,7 +1583,9 @@ class UGRNNCell(rnn_cell_impl.RNNCell):
     with vs.variable_scope(vs.get_variable_scope(),
                            initializer=self._initializer):
       cell_inputs = array_ops.concat([inputs, state], 1)
-      rnn_matrix = _linear(cell_inputs, 2 * self._num_units, True)
+      if self._linear is None:
+        self._linear = _Linear(cell_inputs, 2 * self._num_units, True)
+      rnn_matrix = self._linear(cell_inputs)
 
       [g_act, c_act] = array_ops.split(
           axis=1, num_or_size_splits=2, value=rnn_matrix)
@@ -1638,6 +1650,8 @@ class IntersectionRNNCell(rnn_cell_impl.RNNCell):
     self._num_input_proj = num_in_proj
     self._y_activation = y_activation
     self._reuse = reuse
+    self._linear1 = None
+    self._linear2 = None
 
   @property
   def state_size(self):
@@ -1680,7 +1694,9 @@ class IntersectionRNNCell(rnn_cell_impl.RNNCell):
       if input_size.value != self._num_units:
         if self._num_input_proj:
           with vs.variable_scope("in_projection"):
-            inputs = _linear(inputs, self._num_units, True)
+            if self._linear1 is None:
+              self._linear1 = _Linear(inputs, self._num_units, True)
+            inputs = self._linear1(inputs)
         else:
           raise ValueError("Must have input size == output size for "
                            "Intersection RNN. To fix, num_in_proj should "
@@ -1688,7 +1704,9 @@ class IntersectionRNNCell(rnn_cell_impl.RNNCell):
 
       n_dim = i_dim = self._num_units
       cell_inputs = array_ops.concat([inputs, state], 1)
-      rnn_matrix = _linear(cell_inputs, 2*n_dim + 2*i_dim, True)
+      if self._linear2 is None:
+        self._linear2 = _Linear(cell_inputs, 2*n_dim + 2*i_dim, True)
+      rnn_matrix = self._linear2(cell_inputs)
 
       gh_act = rnn_matrix[:, :n_dim]                           # b x n
       h_act = rnn_matrix[:, n_dim:2*n_dim]                     # b x n
@@ -1825,6 +1843,9 @@ class PhasedLSTMCell(rnn_cell_impl.RNNCell):
     self._period_init_min = period_init_min
     self._period_init_max = period_init_max
     self._reuse = reuse
+    self._linear1 = None
+    self._linear2 = None
+    self._linear3 = None
 
   @property
   def state_size(self):
@@ -1872,14 +1893,18 @@ class PhasedLSTMCell(rnn_cell_impl.RNNCell):
       in_mask_gates.append(c_prev)
 
     with vs.variable_scope("mask_gates"):
+      if self._linear1 is None:
+        self._linear1 = _Linear(in_mask_gates, 2 * self._num_units, True)
+
       mask_gates = math_ops.sigmoid(
-          _linear(in_mask_gates, 2 * self._num_units, True))
+          self._linear1(in_mask_gates))
       [input_gate, forget_gate] = array_ops.split(
           axis=1, num_or_size_splits=2, value=mask_gates)
 
     with vs.variable_scope("new_input"):
-      new_input = math_ops.tanh(
-          _linear([x, h_prev], self._num_units, True))
+      if self._linear2 is None:
+        self._linear2 = _Linear([x, h_prev], self._num_units, True)
+      new_input = math_ops.tanh(self._linear2([x, h_prev]))
 
     new_c = (c_prev * forget_gate + input_gate * new_input)
 
@@ -1888,8 +1913,9 @@ class PhasedLSTMCell(rnn_cell_impl.RNNCell):
       in_out_gate.append(new_c)
 
     with vs.variable_scope("output_gate"):
-      output_gate = math_ops.sigmoid(
-          _linear(in_out_gate, self._num_units, True))
+      if self._linear3 is None:
+        self._linear3 = _Linear(in_out_gate, self._num_units, True)
+      output_gate = math_ops.sigmoid(self._linear3(in_out_gate))
 
     new_h = math_ops.tanh(new_c) * output_gate
 
@@ -2159,6 +2185,8 @@ class GLSTMCell(rnn_cell_impl.RNNCell):
     else:
       self._state_size = rnn_cell_impl.LSTMStateTuple(num_units, num_units)
       self._output_size = num_units
+    self._linear1 = None
+    self._linear2 = None
 
   @property
   def state_size(self):
@@ -2227,7 +2255,9 @@ class GLSTMCell(rnn_cell_impl.RNNCell):
                                        self._group_shape[0]),
              self._get_input_for_group(m_prev, group_id,
                                        self._group_shape[0])], axis=1)
-          R_k = _linear(x_g_id, 4 * self._group_shape[1], bias=False)
+          if self._linear1 is None:
+            self._linear1 = _Linear(x_g_id, 4 * self._group_shape[1], False)
+          R_k = self._linear1(x_g_id)  # pylint: disable=invalid-name
           i_k, j_k, f_k, o_k = array_ops.split(R_k, 4, 1)
 
         i_parts.append(i_k)
@@ -2267,7 +2297,9 @@ class GLSTMCell(rnn_cell_impl.RNNCell):
 
     if self._num_proj is not None:
       with vs.variable_scope("projection"):
-        m = _linear(m, self._num_proj, bias=False)
+        if self._linear2 is None:
+          self._linear2 = _Linear(m, self._num_proj, False)
+        m = self._linear2(m)
 
     new_state = rnn_cell_impl.LSTMStateTuple(c, m)
     return m, new_state

tensorflow/contrib/legacy_seq2seq/python/ops/seq2seq.py
@@ -76,7 +76,7 @@ from tensorflow.python.ops import variable_scope
 from tensorflow.python.util import nest
 
 # TODO(ebrevdo): Remove once _linear is fully deprecated.
-linear = rnn_cell_impl._linear  # pylint: disable=protected-access
+Linear = rnn_cell_impl._Linear  # pylint: disable=protected-access,invalid-name
 
 
 def _extract_argmax_and_embed(embedding,
@@ -645,7 +645,7 @@ def attention_decoder(decoder_inputs,
         query = array_ops.concat(query_list, 1)
       for a in xrange(num_heads):
         with variable_scope.variable_scope("Attention_%d" % a):
-          y = linear(query, attention_vec_size, True)
+          y = Linear(query, attention_vec_size, True)(query)
           y = array_ops.reshape(y, [-1, 1, 1, attention_vec_size])
           # Attention mask is a softmax of v^T * tanh(...).
           s = math_ops.reduce_sum(v[a] * math_ops.tanh(hidden_features[a] + y),
@@ -679,7 +679,9 @@ def attention_decoder(decoder_inputs,
       input_size = inp.get_shape().with_rank(2)[1]
       if input_size.value is None:
         raise ValueError("Could not infer input size from input: %s" % inp.name)
-      x = linear([inp] + attns, input_size, True)
+
+      inputs = [inp] + attns
+      x = Linear(inputs, input_size, True)(inputs)
       # Run the RNN.
       cell_output, state = cell(x, state)
       # Run the attention mechanism.
@@ -691,7 +693,8 @@ def attention_decoder(decoder_inputs,
         attns = attention(state)
 
       with variable_scope.variable_scope("AttnOutputProjection"):
-        output = linear([cell_output] + attns, output_size, True)
+        inputs = [cell_output] + attns
+        output = Linear(inputs, output_size, True)(inputs)
       if loop_function is not None:
         prev = output
       outputs.append(output)

tensorflow/contrib/rnn/python/kernel_tests/core_rnn_cell_test.py
@@ -44,7 +44,7 @@ from tensorflow.python.framework import test_util
 
 
 # pylint: enable=protected-access
-linear = rnn_cell_impl._linear
+Linear = rnn_cell_impl._Linear  # pylint: disable=invalid-name
 
 
 class RNNCellTest(test.TestCase):
@@ -54,20 +54,20 @@ class RNNCellTest(test.TestCase):
       with variable_scope.variable_scope(
           "root", initializer=init_ops.constant_initializer(1.0)):
         x = array_ops.zeros([1, 2])
-        l = linear([x], 2, False)
+        l = Linear([x], 2, False)([x])
         sess.run([variables_lib.global_variables_initializer()])
         res = sess.run([l], {x.name: np.array([[1., 2.]])})
         self.assertAllClose(res[0], [[3.0, 3.0]])
 
         # Checks prevent you from accidentally creating a shared function.
         with self.assertRaises(ValueError):
-          l1 = linear([x], 2, False)
+          l1 = Linear([x], 2, False)([x])
 
         # But you can create a new one in a new scope and share the variables.
         with variable_scope.variable_scope("l1") as new_scope:
-          l1 = linear([x], 2, False)
+          l1 = Linear([x], 2, False)([x])
         with variable_scope.variable_scope(new_scope, reuse=True):
-          linear([l1], 2, False)
+          Linear([l1], 2, False)([l1])
         self.assertEqual(len(variables_lib.trainable_variables()), 2)
 
   def testBasicRNNCell(self):
@@ -141,58 +141,67 @@ class RNNCellTest(test.TestCase):
         self.assertAllClose(res[0], [[0.156736, 0.156736]])
 
   def testBasicLSTMCell(self):
-    with self.test_session() as sess:
-      with variable_scope.variable_scope(
-          "root", initializer=init_ops.constant_initializer(0.5)):
-        x = array_ops.zeros([1, 2])
-        m = array_ops.zeros([1, 8])
-        cell = rnn_cell_impl.MultiRNNCell(
-            [
-                rnn_cell_impl.BasicLSTMCell(2, state_is_tuple=False)
-                for _ in range(2)
-            ],
-            state_is_tuple=False)
-        g, out_m = cell(x, m)
-        expected_variable_names = [
-            "root/multi_rnn_cell/cell_0/basic_lstm_cell/%s:0" %
-            rnn_cell_impl._WEIGHTS_VARIABLE_NAME,
-            "root/multi_rnn_cell/cell_0/basic_lstm_cell/%s:0" %
-            rnn_cell_impl._BIAS_VARIABLE_NAME,
-            "root/multi_rnn_cell/cell_1/basic_lstm_cell/%s:0" %
-            rnn_cell_impl._WEIGHTS_VARIABLE_NAME,
-            "root/multi_rnn_cell/cell_1/basic_lstm_cell/%s:0" %
-            rnn_cell_impl._BIAS_VARIABLE_NAME
-        ]
-        self.assertEqual(
-            expected_variable_names, [v.name for v in cell.trainable_variables])
-        self.assertFalse(cell.non_trainable_variables)
-        sess.run([variables_lib.global_variables_initializer()])
-        res = sess.run(
-            [g, out_m],
-            {x.name: np.array([[1., 1.]]),
-             m.name: 0.1 * np.ones([1, 8])})
-        self.assertEqual(len(res), 2)
-        variables = variables_lib.global_variables()
-        self.assertEqual(expected_variable_names, [v.name for v in variables])
-        # The numbers in results were not calculated, this is just a smoke test.
-        self.assertAllClose(res[0], [[0.24024698, 0.24024698]])
-        expected_mem = np.array([[
-            0.68967271, 0.68967271, 0.44848421, 0.44848421, 0.39897051,
-            0.39897051, 0.24024698, 0.24024698
-        ]])
-        self.assertAllClose(res[1], expected_mem)
-      with variable_scope.variable_scope(
-          "other", initializer=init_ops.constant_initializer(0.5)):
-        x = array_ops.zeros(
-            [1, 3])  # Test BasicLSTMCell with input_size != num_units.
-        m = array_ops.zeros([1, 4])
-        g, out_m = rnn_cell_impl.BasicLSTMCell(2, state_is_tuple=False)(x, m)
-        sess.run([variables_lib.global_variables_initializer()])
-        res = sess.run(
-            [g, out_m],
-            {x.name: np.array([[1., 1., 1.]]),
-             m.name: 0.1 * np.ones([1, 4])})
-        self.assertEqual(len(res), 2)
+    for dtype in [dtypes.float16, dtypes.float32]:
+      np_dtype = dtype.as_numpy_dtype
+      with self.test_session(graph=ops.Graph()) as sess:
+        with variable_scope.variable_scope(
+            "root", initializer=init_ops.constant_initializer(0.5)):
+          x = array_ops.zeros([1, 2], dtype=dtype)
+          m = array_ops.zeros([1, 8], dtype=dtype)
+          cell = rnn_cell_impl.MultiRNNCell(
+              [
+                  rnn_cell_impl.BasicLSTMCell(
+                      2, state_is_tuple=False)
+                  for _ in range(2)
+              ],
+              state_is_tuple=False)
+          self.assertEqual(cell.dtype, None)
+          g, out_m = cell(x, m)
+          # Layer infers the input type.
+          self.assertEqual(cell.dtype, dtype.name)
+          expected_variable_names = [
+              "root/multi_rnn_cell/cell_0/basic_lstm_cell/%s:0" %
+              rnn_cell_impl._WEIGHTS_VARIABLE_NAME,
+              "root/multi_rnn_cell/cell_0/basic_lstm_cell/%s:0" %
+              rnn_cell_impl._BIAS_VARIABLE_NAME,
+              "root/multi_rnn_cell/cell_1/basic_lstm_cell/%s:0" %
+              rnn_cell_impl._WEIGHTS_VARIABLE_NAME,
+              "root/multi_rnn_cell/cell_1/basic_lstm_cell/%s:0" %
+              rnn_cell_impl._BIAS_VARIABLE_NAME
+          ]
+          self.assertEqual(
+              expected_variable_names,
+              [v.name for v in cell.trainable_variables])
+          self.assertFalse(cell.non_trainable_variables)
+          sess.run([variables_lib.global_variables_initializer()])
+          res = sess.run(
+              [g, out_m],
+              {x.name: np.array([[1., 1.]]),
+               m.name: 0.1 * np.ones([1, 8])})
+          self.assertEqual(len(res), 2)
+          variables = variables_lib.global_variables()
+          self.assertEqual(expected_variable_names, [v.name for v in variables])
+          # The numbers in results were not calculated, this is just a
+          # smoke test.
+          self.assertAllClose(
+              res[0], np.array([[0.240, 0.240]], dtype=np_dtype), 1e-2)
+          expected_mem = np.array(
+              [[0.689, 0.689, 0.448, 0.448, 0.398, 0.398, 0.240, 0.240]],
+              dtype=np_dtype)
+          self.assertAllClose(res[1], expected_mem, 1e-2)
+        with variable_scope.variable_scope(
+            "other", initializer=init_ops.constant_initializer(0.5)):
+          # Test BasicLSTMCell with input_size != num_units.
+          x = array_ops.zeros([1, 3], dtype=dtype)
+          m = array_ops.zeros([1, 4], dtype=dtype)
+          g, out_m = rnn_cell_impl.BasicLSTMCell(
+              2, state_is_tuple=False)(x, m)
+          sess.run([variables_lib.global_variables_initializer()])
+          res = sess.run(
+              [g, out_m],
+              {x.name: np.array([[1., 1., 1.]], dtype=np_dtype),
+               m.name: 0.1 * np.ones([1, 4], dtype=np_dtype)})
+          self.assertEqual(len(res), 2)
 
   def testBasicLSTMCellDimension0Error(self):
     """Tests that dimension 0 in both(x and m) shape must be equal."""
@@ -829,7 +838,8 @@ def basic_rnn_cell(inputs, state, num_units, scope=None):
   else:
     with variable_scope.variable_scope(scope, "basic_rnn_cell",
                                        [inputs, state]):
-      output = math_ops.tanh(linear([inputs, state], num_units, True))
+      output = math_ops.tanh(
+          Linear([inputs, state], num_units, True)([inputs, state]))
     return output, output
 
 

tensorflow/contrib/rnn/python/ops/core_rnn_cell.py
@@ -33,7 +33,7 @@ from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.platform import tf_logging as logging
 
 RNNCell = rnn_cell_impl.RNNCell  # pylint: disable=invalid-name
-_linear = rnn_cell_impl._linear  # pylint: disable=invalid-name, protected-access
+_Linear = rnn_cell_impl._Linear  # pylint: disable=invalid-name, protected-access
 _like_rnncell = rnn_cell_impl._like_rnncell  # pylint: disable=invalid-name, protected-access
 
 
@@ -154,6 +154,7 @@ class InputProjectionWrapper(RNNCell):
     self._cell = cell
     self._num_proj = num_proj
     self._activation = activation
+    self._linear = None
 
   @property
   def state_size(self):
@@ -170,7 +171,9 @@ class InputProjectionWrapper(RNNCell):
   def call(self, inputs, state):
     """Run the input projection and then the cell."""
     # Default scope: "InputProjectionWrapper"
-    projected = _linear(inputs, self._num_proj, True)
+    if self._linear is None:
+      self._linear = _Linear(inputs, self._num_proj, True)
+    projected = self._linear(inputs)
     if self._activation:
       projected = self._activation(projected)
     return self._cell(projected, state)
@@ -208,6 +211,7 @@ class OutputProjectionWrapper(RNNCell):
     self._cell = cell
     self._output_size = output_size
     self._activation = activation
+    self._linear = None
 
   @property
   def state_size(self):
@@ -224,7 +228,9 @@ class OutputProjectionWrapper(RNNCell):
   def call(self, inputs, state):
     """Run the cell and output projection on inputs, starting from state."""
     output, res_state = self._cell(inputs, state)
-    projected = _linear(output, self._output_size, True)
+    if self._linear is None:
+      self._linear = _Linear(output, self._output_size, True)
+    projected = self._linear(output)
     if self._activation:
       projected = self._activation(projected)
     return projected, res_state

commit 795fae68483ca2f02855dc7c0064e077db55e15f

contrib/rnn: Fix #16703 (Bug introduced in https://github.com/tensorflow/tensorflow/commit/940ed77590739f524d1478056c74e93cd5f59c7f)
Kudos to @akhti for pointing this out.

PiperOrigin-RevId: 184570448

tensorflow/contrib/rnn/python/ops/rnn_cell.py
@@ -2285,7 +2285,7 @@ class GLSTMCell(rnn_cell_impl.RNNCell):
     else:
       self._state_size = rnn_cell_impl.LSTMStateTuple(num_units, num_units)
       self._output_size = num_units
-    self._linear1 = None
+    self._linear1 = [None] * number_of_groups
     self._linear2 = None
 
   @property
@@ -2359,9 +2359,11 @@ class GLSTMCell(rnn_cell_impl.RNNCell):
                                             self._group_shape[0])
               ],
               axis=1)
-          if self._linear1 is None:
-            self._linear1 = _Linear(x_g_id, 4 * self._group_shape[1], False)
-          R_k = self._linear1(x_g_id)  # pylint: disable=invalid-name
+          linear = self._linear1[group_id]
+          if linear is None:
+            linear = _Linear(x_g_id, 4 * self._group_shape[1], False)
+            self._linear1[group_id] = linear
+          R_k = linear(x_g_id)  # pylint: disable=invalid-name
           i_k, j_k, f_k, o_k = array_ops.split(R_k, 4, 1)
 
         i_parts.append(i_k)